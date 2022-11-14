2019dbd9532a7ea3963a4dcbf42501cc293ac69c
Dmitrii Zolotov <dmitrii.zolotov@gmail.com>
2022-11-14 20:23:07 +0300

    Update recipe.dart

--- lib/controllers/recipe.dart
+++ lib/controllers/recipe.dart
@@ -6,3 +6,7 @@ class RecipeController extends ManagedObjectController<Recipe> {
   RecipeController(ManagedContext context) : super(context);
 }
 
+clas RecipeStepController extends ManagedObjectController<RecipeStep> {
+  RecipeController(ManagedContext context) : super(context);
+}
+
