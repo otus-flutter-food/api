fb37d499f0f0fc8986ebc9703af064039efd559f
vachtung-gigabidze <dmitry.zyadik@gmail.com>
2022-12-07 21:00:07 +0500

    add favorites:userid

--- lib/controllers/favorite.dart
+++ lib/controllers/favorite.dart
@@ -1,7 +1,30 @@
+import 'dart:developer';
+
 import 'package:conduit/conduit.dart';
+import 'package:foodapi/model/recipe.dart';
 
 import '../model/favorite.dart';
 
 class FavoriteController extends ManagedObjectController<Favorite> {
   FavoriteController(ManagedContext context) : super(context);
+}
+
+class FavoritesController extends ResourceController {
+  FavoritesController(this.context);
+
+  final ManagedContext context;
+
+  @Operation.get('id')
+  Future<Response> getFavorites() async {
+    final id = int.parse(request?.path.variables['id'] ?? "");
+    final query = Query<Recipe>(context)..join(set: (f) => f.favoriteRecipes);
+
+    final recipe = await query.fetch();
+    final filterRecipe = recipe
+        .where((r) => r.favoriteRecipes!.any((f) => f.user?.id == id))
+        .toList();
+    final Response response = Response.ok(filterRecipe);
+
+    return response;
+  }
 }
