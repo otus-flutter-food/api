b4c2432e0cabc78df241d4767a8c2cd61a7625e1
Dmitrii Zolotov <dmitrii.zolotov@gmail.com>
2022-12-10 23:51:23 +0300

    Comment photo is optional

---
+++ migrations/00000003_unnamed.migration.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'package:conduit/conduit.dart';   
+
+class Migration3 extends Migration { 
+  @override
+  Future upgrade() async {
+   		database.alterColumn("_Comment", "photo", (c) {c.isNullable = true;});
+  }
+  
+  @override
+  Future downgrade() async {}
+  
+  @override
+  Future seed() async {}
+}
+    
