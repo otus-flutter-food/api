f5487498a2d70ee6e6318608bdc4abcf1409be60
Dmitrii Zolotov <dmitrii.zolotov@gmail.com>
2025-07-29 16:17:30 +0300

    Add diagnostic endpoint and configurable port
    - Add /test endpoint to verify database connection
    - Make port configurable via PORT environment variable
    - Import dart:io for Platform access in main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:conduit_core/conduit_core.dart';
 import 'package:foodapi/foodapi.dart';
 
 Future main() async {
   final app = Application<FoodapiChannel>()
     ..options.configurationFilePath = "config.yaml"
-    ..options.port = 8888
+    ..options.port = int.parse(Platform.environment['PORT'] ?? '8888')
     ..options.address = InternetAddress.anyIPv4;
 
   await app.startOnCurrentIsolate();
