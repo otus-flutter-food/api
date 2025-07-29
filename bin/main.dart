93e017f9035e37c628383f6cddd377dcce258b07
Dmitrii Zolotov <dmitrii.zolotov@gmail.com>
2025-07-29 14:57:04 +0300

    Configure API to listen on all interfaces (0.0.0.0)
    - Add InternetAddress.anyIPv4 to bind to 0.0.0.0
    - Update startup message to show bind address
    - Fixes connectivity issues when running in Docker

--- bin/main.dart
+++ bin/main.dart
@@ -4,10 +4,11 @@ import 'package:foodapi/foodapi.dart';
 Future main() async {
   final app = Application<FoodapiChannel>()
     ..options.configurationFilePath = "config.yaml"
-    ..options.port = 8888;
+    ..options.port = 8888
+    ..options.address = InternetAddress.anyIPv4;
 
   await app.startOnCurrentIsolate();
 
-  print("Application started on port: ${app.options.port}.");
+  print("Application started on ${app.options.address.address}:${app.options.port}.");
   print("Use Ctrl-C (SIGINT) to stop running the application.");
 }
